555ea774137db100785b77afbc0a7c537f09e72c
n-els <78740982+n-els@users.noreply.github.com>
2022-01-29 15:26:58 +0100

    Create JSON2MongoDB.js

---
+++ JSON2MongoDB.js
@@ -0,0 +1,54 @@
+// IMPORT
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './../../config.env' });
+const fs = require('fs');
+const Tour = require('./../../models/tourModel');
+
+// CREATE THE DATABASE AND SET PASSWORD FOR THE FROM THE ENVIRONMENT VARIABLES
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// CONNECT TO THE DATABASE
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE AND PARSE/CONVERT THE DATA TO JAVASCRIPT OBJECT
+const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+
+// IMPORT DATA INTO DATABASE
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// DELETE ALL DATA FROM COLLECTION
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+console.log(process.argv);
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
